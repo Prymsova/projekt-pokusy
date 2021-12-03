229c55069e3eb19faab027a45f8ed35394d083b0
Anna Prymšová <anca7@centrum.cz>
2021-12-03 22:43:18 +0100

    prvni commit, rozpracovane hmtl

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat css/ do _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+  // Zkopírovat icons/ do _site/icons/
+	eleventyConfig.addPassthroughCopy("icons");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
